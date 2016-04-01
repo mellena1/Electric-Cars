5a33a46d58d848ee6f78c34a02a32e31299fd6b0
Bradley Rawosn Jr. <Rawsonb@wit.edu>
2016-03-31 22:40:47 -0400

    Andrew is a slut

---
+++ src/electicCars/Cars.java
@@ -0,0 +1,11 @@
+package electicCars;
+
+public abstract class Cars {
+	
+	public Cars() {}
+	
+	public abstract void milesLeft();
+	
+	public abstract void chargeTime();
+
+}
---
+++ src/electicCars/Graphable.java
@@ -0,0 +1,11 @@
+package electicCars;
+
+import java.util.ArrayList;
+
+public interface Graphable {
+	
+	ArrayList<Integer> data = new ArrayList<Integer>();
+	
+	void storeData();
+	
+}
