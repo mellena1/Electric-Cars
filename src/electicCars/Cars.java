efca82ee5915672cbf76bd7d1ea79750572ca2c0
Bradley Rawosn Jr. <Rawsonb@wit.edu>
2016-04-04 19:57:27 -0400

    the array you asked for

--- src/electicCars/Cars.java
+++ src/electicCars/Cars.java
@@ -2,6 +2,13 @@ package electicCars;
 
 public abstract class Cars {
 	
+	String[] carNames = new String[] 
+			{"BMWi3","ChevroletBolt","ChevroletSparkEV",
+			"Fiat500e","FordFocusElectric","KiaSoulEV",
+			"MercedesBClassElectricDrive","MitsubishiiMiEV","NissanLeaf",
+			"SmartElectricDrive","TeslaModel3","TeslaModelS",
+			"TeslaModelX","VolkswagenEGolf"};
+	
 	public abstract Integer milesLeft(Integer powerLeft);
 	
 	public abstract Integer chargeTime(Integer powerLeft);
